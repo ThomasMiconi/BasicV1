9ba9472a5da57348f4dbd28e458abf0484fcc739
Thomas Miconi <thomas.miconi@gmail.com>
2016-09-27 14:37:42 -0400

    More comments, included readme.md

---
+++ README.md
@@ -0,0 +1,16 @@
+This code implements super-simple, super-fast Hebbian learning of nice V1-like simple-cell receptive fields.
+
+25 lines of Python, runs in a couple seconds (though the RF look even smoother if you run it for longer with a smaller learning rate).
+
+
+This code uses the "instar" Hebbian rule (see e.g. [http://cns-pc62.bu.edu/cn510/Papers/stability.pdf](Vasilkoski et al. IJCNN 2011)), SoftMax competition between output cells, and adaptive thresholds to drive firing rates towards 1/NBCELLS (though the actual firing rates vary quite a lot).
+
+
+The resulting firing is sparse, and most often only one cell fires. But sometimes several cells have significant firing (rarely more than 3 though). 
+
+The main program is `hebb.py`, which can be run as-is. `makepatches.py` generates the input patches (it requires a directory with many natural images in it - see code). `plotRFs.py` plots the learned receptive fields. 
+
+This repository also includes pre-computed patches of z-scored natural images as inputs, stored as linearized vectors in patches.npy (see `makepatches.py`).
+
+To plot the resulting RFs, after running `hebb.py`, run `plotRFs.py`.
+
--- makepatches.py
+++ makepatches.py
@@ -9,7 +9,7 @@ CROPMARGIN = 13
 CROPSIZE = RFSIZE + 2* CROPMARGIN
 NBFRAMES = 100000
 
-fnames = glob.glob('../stdp/images/ImageNet2/images/*')
+fnames = glob.glob('../stdp/images/ImageNet2/images/*')   # Replace with your own directory containing many natural images.
 images = []
 imageso = []
 
@@ -19,6 +19,8 @@ while 1:
     if nbimages % 10 == 0:
         print nbimages
     while 1:
+
+        # Let's try to load an image
         try:
             fname = np.random.choice(fnames)
             im = sp.ndimage.imread(fname)
@@ -35,19 +37,26 @@ while 1:
         print "Too small image"
         continue
 
+    # We extract a small patch from the image, of size CROPSIZE * CROPSIZE
+    # CROPSIZE is larger than RFSIZE, because we want ton include some margin around the patch to avoid edge effects during the filtering.
     xpos = np.random.randint(im.shape[0]-CROPSIZE-1)
     ypos = np.random.randint(im.shape[1]-CROPSIZE-1)
     imcrop = im[xpos:xpos+CROPSIZE, ypos:ypos+CROPSIZE]
     #imageso.append(imcrop)
+    # Difference-of-Gaussians filtering emulates the processing of retinal / LGN cells, decorrelates nearby pixels, and also flattens the spectrum.
     imfilt = sp.ndimage.filters.gaussian_filter(imcrop, 1.0) - sp.ndimage.filters.gaussian_filter(imcrop, 2.0) 
     imfilt = imfilt[CROPMARGIN:CROPMARGIN+RFSIZE, CROPMARGIN:CROPMARGIN+RFSIZE].flatten()
+
+    # Z-scoring!
     imfilt -= np.mean(imfilt)
     if np.std(imfilt) < 1.0:
         print "Not enough variation in image."
         continue
     imfilt /= np.std(imfilt)
-    imfilt *= 19.0
-    imfilt = np.rint(np.clip(imfilt, -127, 127)).astype('int8')
+
+    # We want to convert this to 8-bit signed integers, to save space
+    imfilt *= 19.0    # Expand the range as much as possible while keeping the vast majority of the pixels  within the -127:127 interval
+    imfilt = np.rint(np.clip(imfilt, -127, 127)).astype('int8')   # There should be very little clipping
     images.append(imfilt)
     #print
     nbimages += 1
@@ -55,9 +64,6 @@ while 1:
         break
 
 aimages = np.array(images) 
-#trimmin = np.percentile(aimages, 1)
-#trimmax = np.percentile(aimages, 99)
-#np.clip(aimages, trimmin, trimmax, out=aimages)  # alters the mean and variance...
 np.save('patches.npy', aimages)
 
 
